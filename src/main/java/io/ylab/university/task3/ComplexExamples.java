ec59077fd4026bbcc1ee5885180989e90f3ecfd1
Kolesov Sergey <s.kolesov78@gmail.com>
2022-09-13 22:11:06 +0300

    Removed unnecessary imports

--- src/main/java/io/ylab/university/task3/ComplexExamples.java
+++ src/main/java/io/ylab/university/task3/ComplexExamples.java
@@ -1,12 +1,8 @@
 package io.ylab.university.task3;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
